1450f19966e88f764b4fefbc298db06be9bcc902
William Samuelson <william.samuelson@gmail.com>
2025-10-15 11:47:58 +0200

    simplify test for GS majoranas

--- scripts/kitaev_analytics.jl
+++ scripts/kitaev_analytics.jl
@@ -17,8 +17,8 @@ vals, vecs = ground_states_arnoldi(ham, HS)
 @test abs(vals[2] - vals[1]) < 1e-10
 o = vcat(vecs[1], zero(vecs[2]))
 e = vcat(zero(vecs[1]), vecs[2])
-y = o*e' + hc
-yt = 1im*o*e' + hc
+y = e*o' + hc
+yt = 1im*e*o' + hc
 
 maj_labels = vec(permutedims(collect(Base.product(S, (:+, :-)))))
 fmats = fermions(HS)
@@ -28,13 +28,13 @@ fmats = fermions(HS)
 @test Γ[N, :-] ≈ 1im * (fmats[N] - fmats[N]')
 Q = 1 / 2^(N - 1) * prod(I - 1im * Γ[j, :-] * Γ[j + 1, :+] for j in 1:N-1)
 @test Q ≈ e * e' + o * o'
-@test y ≈ Γ[1, :+] * Q
-@test yt ≈ Γ[N, :-] * Q
+@test (y ≈ Γ[1, :+] * Q) || (y ≈ -Γ[1, :+] * Q) # the sign of |e> and |o> is arbitrary
+@test yt ≈ Γ[N, :-] * Q || (yt ≈ -Γ[N, :-] * Q)
 
 Hjs = [hilbert_space(j:j, ParityConservation()) for j in S]
 yjs = [partial_trace(y, HS => Hj) for Hj in Hjs]
-@test yjs[1] ≈ partial_trace(Γ[1, :+], HS => Hjs[1]) * dim(Hjs[1]) / dim(HS)
+@test norm(yjs[1]) ≈ sqrt(2)
 @test all(norm.(yjs[2:N]) .< 1e-10)
 ytjs = [partial_trace(yt, HS => Hj) for Hj in Hjs]
-@test ytjs[N] ≈ partial_trace(Γ[N, :-], HS => Hjs[N]) * dim(Hjs[N]) / dim(HS)
+@test norm(ytjs[N]) ≈ sqrt(2)
 @test all(norm.(ytjs[1:N - 1]) .< 1e-10)
