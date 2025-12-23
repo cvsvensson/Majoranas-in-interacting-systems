e0a2c8a78a55431758aea68fb09108f6a1d38e68
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2025-12-23 15:03:44 +0100

    spinful example, conductance, non-perturbative coupling, random coupling

---
+++ scripts/energy_splitting_non_perturbative.jl
@@ -0,0 +1,111 @@
+using DrWatson
+@quickactivate :ManybodyMajoranas
+using Folds
+using UnPack, CairoMakie, MakiePublication, LaTeXStrings
+function calculate_bounds(hamiltonians, spaces, q)
+    @unpack HS = spaces
+    canon_hams = canonicalize_hamiltonians(hamiltonians, spaces)
+    @unpack hS0, hS, hB, hRB = canon_hams
+    _, vecs = ground_states_arnoldi(hS0, HS)
+    gs_odd = vcat(vecs[1], zero(vecs[2]))
+    gs_even = vcat(zero(vecs[1]), vecs[2])
+
+    reduced = reduced_majoranas_properties(gs_even, gs_odd, HS, HR, FrobeniusGauge(); q)
+    return calculate_bounds(reduced, hamiltonians, spaces, q)
+end
+
+function calculate_bounds(reduced, hamiltonians, spaces, q)
+    @unpack HS, HB, HR, HSB, HRB = spaces
+    p = conjugate_norm(q)
+    canon_hams = canonicalize_hamiltonians(hamiltonians, spaces)
+    @unpack hS0, hS, hB, hRB = canon_hams
+    heff = effective_hamiltonian(canon_hams, spaces, (reduced.γmin, reduced.γmax))
+    if abs(heff.effops.ε) > 1e-6
+        @warn "ε > 1e-6" (heff.effops.ε)
+    end
+    vals_full, _ = blockeigen(embed(hS0, HS => HSB) + embed(hRB, HRB => HSB) + embed(hB, HB => HSB), HSB)
+    vals, vecs = blockeigen(heff.total_ham, spaces.HgsB)
+    n = div(size(vecs, 2), 2) # number of odd/even states
+    odd_coupling, even_coupling = decompose_coupling(hRB, HRB, HR, HB)
+    even_norm = schatten_norm(even_coupling, p)
+    odd_norm = schatten_norm(odd_coupling, p)
+    odd_states, even_states = vecs.blocks
+    γgsBmin, _ = [embed(γ, spaces.Hgs => spaces.HgsB) for γ in heff.γgs]
+    overlaps1 = abs.(odd_states' * γgsBmin[1:n, n+1:2n] * even_states)
+    OEs = [E * O' for (O, E) in Base.product(eachcol(vecs[:, 1:n]), eachcol(vecs[:, n+1:end]))]
+    OEBnorms = [schatten_norm(partial_trace(OE, spaces.HgsB => spaces.HB), q) for OE in OEs]
+    pairs = map(CartesianIndex, enumerate(map(v -> argmax(v), eachrow(overlaps1))))
+    δEs = map(es -> -(es...), Base.product(vals[1:n], vals[n+1:2n]))
+    δEs_full = abs(vals_full[1] - vals_full[div(length(vals_full), 2)+1])
+    np_bound = (reduced.LD * even_norm .+ reduced.LFmin * odd_norm * OEBnorms) ./ overlaps1
+    p_bound = sqrt(dim(spaces.HB)) * (reduced.LD * even_norm + reduced.LFmin * odd_norm)
+    (; heff, δEs, δEs_full, p_bound, np_bound, even_norm, odd_norm, vals, vecs, pairs, overlaps1, OEBnorms)
+end
+
+##
+@fermions f
+N = 8
+S = 1:N
+B = length(S) .+ (1:1)
+R = last(S):last(S)
+r = only(R)
+b = only(B)
+qn = ParityConservation()
+spaces = hilbert_spaces(S, R, B, qn)
+@unpack HS, HB, HR, HSB, HRB = spaces
+##
+@time params = energy_splitting_parameters(HS)
+# params = (; params..., μ=round.(params.μ; digits=4))
+symham = kitaev_hamiltonian(f, HS; params...)
+hS = matrix_representation(symham, HS)
+vals, vecs = blockeigen(hS, HS)
+nSB = div(size(vecs, 2), 2)
+gs_odd = vecs[:, 1]
+gs_even = vecs[:, nSB+1]
+exc_gap = minimum([abs(vals[2] - vals[1]), abs(vals[nSB+2] - vals[nSB+1])]) # excitation gap
+gapratio = abs((vals[1] - vals[nSB+1]) / exc_gap)
+@assert gapratio < 1e-8 "Not degenerate: gapratio = $gapratio"
+
+## vary dot level
+λ = 0.5 * global_parameters.t
+θ = pi / 6
+tc = λ * cos(θ)
+Δc = λ * sin(θ)
+Uc = λ
+hRBsym = tc * f[r]' * f[b] + Δc * f[r] * f[b] + hc +
+         Uc * f[b]' * f[b] * f[r]' * f[r]
+hRB = matrix_representation(hRBsym, spaces.HRB)
+ϵs = 2 * λ * range(-1, 1, 50)
+reduced = reduced_majoranas_properties(gs_even, gs_odd, HS, HR, FrobeniusGauge(); q=2);
+@time energy_splitting_data = Folds.map(ϵs) do ϵ
+    hB = matrix_representation(ϵ * f[only(B)]' * f[only(B)], spaces.HB)
+    hamiltonians = (; hS0=hS, hS=hS, hB=hB, hRB=hRB)
+    calculate_bounds(reduced, hamiltonians, spaces, 2)
+end;
+##
+δEs = [abs(d.δEs[1, 1]) for d in energy_splitting_data]
+δEs_full = [abs(d.δEs_full[1, 1]) for d in energy_splitting_data]
+normalization = λ
+npbounds = [d.np_bound[1, 1] for d in energy_splitting_data]
+pbounds = [d.p_bound for d in energy_splitting_data]
+energy_splitting_fig = with_theme(theme_aps()) do
+    fig = Figure(size=150 .* (1.5, 1), figure_padding=5)
+    ax = Axis(fig[1, 1]; xlabel=L"\varepsilon_d/ λ", limits=(nothing, (0, 1.1 * pbounds[1] / normalization)))
+    colors = [Cycled(2), Cycled(4), Cycled(1)]
+    lines!(ax, ϵs ./ λ, pbounds ./ normalization, label=LaTeXString("Eq. (36)"); linestyle=(:dot, :dense), color=colors[1])
+    lines!(ax, ϵs ./ λ, npbounds ./ normalization, label=LaTeXString("Eq. (35)"); linestyle=:dash, color=colors[2])
+    lines!(ax, ϵs ./ λ, δEs ./ normalization, label=L"|\delta E| / λ"; linestyle=nothing, color=colors[3])
+    lines!(ax, ϵs ./ λ, δEs_full ./ normalization, label=L"|\delta Efull| / λ"; linestyle=nothing, color=:black)
+    text!(fig.scene, 0.03, 0.84; text=LaTeXString("\\frac{E}{λ}"), space=:relative, fontsize=10)
+    # text!(ax, 0.2, 0.7; text=L"Q_o = %$(round(reduced.LFmin; digits =3))", space=:relative)
+    # text!(ax, 0.2, 0.55; text=L"Q_e = %$(round(reduced.LD; digits =3))", space=:relative)
+    axislegend(ax; position=(0.9, 0.75))
+    fig
+end
+##
+save(plotsdir("energy_splitting_comparison_$N.pdf"), energy_splitting_fig)
+save(plotsdir("energy_splitting_comparison_$N.png"), energy_splitting_fig, px_per_unit=1.2)
+
+##
+reduced.LFmin
+reduced.LD
